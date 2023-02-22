00abf4a4fa72e6bc46032ec4abd5e3efd09abd81
Th4Dude <gallottimaurizio@yahoo.it>
2023-02-22 21:59:35 +0200

    Done

--- js/main.js
+++ js/main.js
@@ -15,36 +15,49 @@ let prezzoFinale;
 
 let sconti = prezzoFinale
 
-if (eta === 'minorenni') {  
+if (eta === 'minorenni') {
     prezzoFinale = prezzoIntero * 0.8;
     sconti = 'Sconto Minorenni'
 
-} else if (eta === 'over65') { 
+} else if (eta === 'over65') {
     prezzoFinale = prezzoIntero * 0.6;
     sconti = 'Sconto Over 65'
 
 } else {
     prezzoFinale = prezzoIntero;
     sconti = 'Biglietto Standard'
-    
+
 }
 
 /* if */
 
+const biglietto = document.querySelector('.biglietto');
+
 const inputButton = document.getElementById('prezzo');
+
+let carrozzaNumero = Math.floor((Math.random() * 30) + 1);
+
+let codiceNumero = Math.floor((Math.random() * 10000) + 1);
 
 /* Funzioni */
 
-inputButton.addEventListener('click', 
-    function() {
+inputButton.addEventListener('click',
+
+    function () {
+
         document.getElementById('costo_biglietto').innerHTML = `${prezzoFinale.toFixed(2)} €`
         document.getElementById('nome_passegero').innerHTML = `${nome}`
         document.getElementById('offerta').innerHTML = `${sconti}`
+        document.getElementById("random").innerHTML = carrozzaNumero;
+        document.getElementById("codice").innerHTML = codiceNumero;
+        biglietto.classList.add('biglietto_appare');
+
     }
 
 )
 
 /* Funzioni */
+
 
 
 
