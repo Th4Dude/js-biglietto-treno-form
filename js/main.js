9fdc84eb2695f3ae6414f9509ddf36accdc4703e
Th4Dude <gallottimaurizio@yahoo.it>
2023-02-22 20:07:37 +0200

    Procedo con html css

--- js/main.js
+++ js/main.js
@@ -10,28 +10,41 @@ let prezzoIntero = km * 0.21;
 /* variabili */
 
 /* if */
- 
+
 let prezzoFinale;
+
+let sconti = prezzoFinale
 
 if (eta === 'minorenni') {  
     prezzoFinale = prezzoIntero * 0.8;
+    sconti = 'Sconto Minorenni'
+
 } else if (eta === 'over65') { 
     prezzoFinale = prezzoIntero * 0.6;
+    sconti = 'Sconto Over 65'
+
 } else {
     prezzoFinale = prezzoIntero;
+    sconti = 'Biglietto Standard'
+    
 }
+
 /* if */
 
 const inputButton = document.getElementById('prezzo');
+
+/* Funzioni */
 
 inputButton.addEventListener('click', 
     function() {
         document.getElementById('costo_biglietto').innerHTML = `${prezzoFinale.toFixed(2)} €`
         document.getElementById('nome_passegero').innerHTML = `${nome}`
+        document.getElementById('offerta').innerHTML = `${sconti}`
     }
 
 )
 
+/* Funzioni */
 
 
 
